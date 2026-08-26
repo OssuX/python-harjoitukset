8140b50ac2bdde6b9c1b1786dd40df305de2cc49
Oskari Savikko <oskari.savikko@gmail.com>
2026-08-26 17:52:24 +0300

    tehtäv3

---
+++ mod03/tehtävä3.py
@@ -0,0 +1,8 @@
+kanta = float(input("Anna suorakulmion kanta:"))
+korkeus = float(input("Anna suorkulmion korkeus:"))
+
+piiri = 2 * (kanta + korkeus)
+pinta_ala = kanta * korkeus
+
+print("Suorakulmion piiri on {}".format(piiri))
+print("Suorakulmion pinta_ala on {}".format(pinta_ala))
